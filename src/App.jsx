cb7714fcb304b19c9871f022ecbd60010e6187d5
Ayush <aayushpandey07080603@gmail.com>
2025-03-18 14:16:07 +0530

    Fix line endings

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ProductProvider } from './contexts/ProductContext';  // Import the ProductProvider
-import HomeScreen from './components/HomeScreen';  // Import HomeScreen component
-import ProductDetails from './components/ProductDetails';  // Import ProductDetails component
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { ProductProvider } from './contexts/ProductContext';  
+import HomeScreen from './components/HomeScreen';  
+import ProductDetails from './components/ProductDetails';  
 
 function App() {
   return (
-    <ProductProvider> {/* Wrap the application with ProductProvider */}
-      <Router> {/* Setting up Router for navigation */}
-        <Routes> {/* Define the routes for the app */}
-          <Route path="/" element={<HomeScreen />} />  {/* Home screen route */}
-          <Route path="/product/:id" element={<ProductDetails />} />  {/* Product details route */}
-        </Routes>
-      </Router>
-    </ProductProvider>
+    <React.StrictMode>
+      <ProductProvider> 
+        <Router> 
+          <Routes> 
+            <Route path="/" element={<HomeScreen />} />  
+            <Route path="/product/:id" element={<ProductDetails />} />  
+          </Routes>
+        </Router>
+      </ProductProvider>
+    </React.StrictMode>
   );
 }
 
